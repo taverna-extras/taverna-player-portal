e1746bfb5579292d027ecfa444d685165295e9e0
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-02-15 17:56:44 +0000

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Rails4app::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Needed for devise
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
