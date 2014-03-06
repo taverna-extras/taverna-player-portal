847d9287a91c448babb58c681f02363782db427c
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-03-06 12:00:01 +0000

    Added 'name' field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304124331) do
+ActiveRecord::Schema.define(version: 20140306115018) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20140304124331) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
