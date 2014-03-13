738d61385380f543a86220a0559dcb7da3078a0c
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-03-13 12:14:21 +0000

    Remove unused tests

--- test/helpers/workflow_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class WorkflowHelperTest < ActionView::TestCase
-end
--- test/models/workflow_input_port_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkflowInputPortTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
