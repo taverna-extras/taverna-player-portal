f1a37de14d83928b41ba1ddee4ae95fa1ae076b8
Robert Haines <rhaines@manchester.ac.uk>
2014-10-27 15:37:55 +0000

    Move the RunsController tests into the correct namespace.
    RunsController is overridden from Taverna Player so the tests should be in that
    namespace. It doesn't make much difference in practise but good for consistency.

---
+++ test/controllers/taverna_player/runs_controller_test.rb
@@ -0,0 +1,130 @@
+require 'test_helper'
+
+module TavernaPlayer
+  class RunsControllerTest < ActionController::TestCase
+
+    test "should get all runs" do
+      get :index, :use_route => :taverna_player
+
+      assert_not_nil assigns(:runs)
+      assert_response :success
+    end
+
+    test "should get create form" do
+      get :new, :workflow_id => create(:workflow).id, :use_route => :taverna_player
+
+      assert_response :success
+    end
+
+    test "should get edit form" do
+      run = create(:run)
+      sign_in run.user
+
+      get :edit, :id => run.id
+
+      assert_response :success
+    end
+
+    test "shouldn't get edit form if unauthorized" do
+      run = create(:run)
+
+      get :edit, :id => run.id
+
+      assert_response :unauthorized
+    end
+
+    test "should get edit form if admin" do
+      run = create(:run)
+      sign_in create(:admin)
+
+      get :edit, :id => run.id
+
+      assert_response :success
+    end
+
+    test "should create run" do
+      user = create(:user)
+      sign_in user
+
+      post :create, :run => {
+        :name => 'Test', :workflow_id => create(:workflow).id,
+        :inputs_attributes => [{:name => 'simple_input', :value => 'hello'}]
+      }, :use_route => :taverna_player
+
+      assert_redirected_to assigns(:run)
+      assert_empty assigns(:run).errors
+    end
+
+    test "should create run as anonymous user" do
+      post :create, :run => {
+        :name => 'Test', :workflow_id => create(:workflow).id,
+        :inputs_attributes => [{:name => 'simple_input', :value => 'hello'}]
+      }, :use_route => :taverna_player
+
+      assert_redirected_to assigns(:run)
+      assert_empty assigns(:run).errors
+    end
+
+    test "should update run" do
+      run = create(:run)
+      sign_in run.user
+
+      patch :update, :id => run.id, :run => {:name => 'New title'}
+
+      assert_redirected_to run_url(run)
+      assert_equal 'New title', assigns(:run).name
+    end
+
+    test "shouldn't update run if unauthorized" do
+      run = create(:run)
+      sign_in create(:user)
+
+      old_title = run.name
+
+      patch :update, :id => run.id, :run => {:name => 'New title'}
+
+      assert_response :unauthorized
+      assert_equal old_title, assigns(:run).name
+    end
+
+    test "should update run if admin" do
+      run = create(:run)
+      sign_in create(:admin)
+
+      patch :update, :id => run.id, :run => {:name => 'New title'}
+
+      assert_redirected_to run_url(run)
+      assert_equal 'New title', assigns(:run).name
+    end
+
+    test "should delete run" do
+      run = create(:run)
+      sign_in run.user
+
+      request.env["HTTP_REFERER"] = run_url(run)
+      delete :destroy, :id => run.id, :use_route => :taverna_player
+
+      assert_redirected_to run_url(run)
+    end
+
+    test "shouldn't delete run if unauthorized" do
+      run = create(:run)
+      sign_in create(:user)
+
+      delete :destroy, :id => run.id, :use_route => :taverna_player
+
+      assert_response :unauthorized
+    end
+
+    test "should delete run if admin" do
+      run = create(:run)
+      sign_in create(:admin)
+
+      request.env["HTTP_REFERER"] = run_url(run)
+      delete :destroy, :id => run.id, :use_route => :taverna_player
+
+      assert_redirected_to run_url(run)
+    end
+
+  end
+end
