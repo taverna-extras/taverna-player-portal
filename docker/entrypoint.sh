a8ae28e4ac40761ea3eb997a9d05d22d97e80373
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-06-12 12:18:10 +0100

    Fixed delayed job not starting and added variables for taverna server address and credentials

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-/taverna-player-portal/script/delayed_job start
+sed -i 's,config.server_address *=.*,config.server_address = "'"$TAVERNA_SERVER_URL"'",'  /taverna-player-portal/config/initializers/taverna_server.rb
+sed -i 's,config.server_username *=.*,config.server_username = "'"$TAVERNA_SERVER_USERNAME"'",'  /taverna-player-portal/config/initializers/taverna_server.rb
+sed -i 's,config.server_password *=.*,config.server_password = "'"$TAVERNA_SERVER_PASSWORD"'",'  /taverna-player-portal/config/initializers/taverna_server.rb
+
+/taverna-player-portal/script/delayed_job --queue=player start
 
 exec "$@"
 
